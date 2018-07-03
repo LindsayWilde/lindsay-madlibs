18e65aed1b5f8f268491a298a1ea566b8c3ba8e3
LindsayWilde <lindsaywilde439@gmail.com>
2018-07-03 10:34:39 -0600

    created Conent component to generate story

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Input from './input';
+import Content from './content';
 
 class Card extends Component {
 
@@ -32,7 +33,6 @@ class Card extends Component {
 
     handleInputChange(event) {
         this.setState({[event.target.name]: event.target.value})
-        console.log(this.state)
     }
 
 
@@ -65,11 +65,7 @@ class Card extends Component {
                 {
                     inputData.map(data => Input((data), this.handleInputChange ))
                 }
-                {
-                    inputData.map((data, index) => {
-                        return <div key={index}>{this.title}</div>
-                    })
-                }
+                <Content data={this.state}/>
             </div>
         )
     }
